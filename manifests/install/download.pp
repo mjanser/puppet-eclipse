c56ea6cf45851f682532028d7206acd43a8f8fdd
Sam Weston <admin@samuelweston.com>
2016-04-04 20:15:06 +0100

    Response to Martin's pull request comments.

--- manifests/install/download.pp
+++ manifests/install/download.pp
@@ -5,9 +5,7 @@
 # Sample Usage:
 #
 #  include eclipse::install::download
-#--
-# TODO: Enable desktop shortcut for given users.
-#++
+#
 class eclipse::install::download (
   $package         = 'standard',
   $release_name    = 'kepler',
@@ -59,7 +57,7 @@ class eclipse::install::download (
   file { '/usr/share/applications/opt-eclipse.desktop':
     ensure  => $ensure,
     content => template('eclipse/opt-eclipse.desktop.erb'),
-    mode    => '644',
+    mode    => 644,
     require => Archive[$filename]
   }
 
